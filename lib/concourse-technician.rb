0dfedbb53d9085d45bf22a09aef08397322aa249
Chris Olstrom <chris@olstrom.com>
2016-09-15 15:34:26 -0700

    Add VolumeReaper to require wrapper

--- lib/concourse-technician.rb
+++ lib/concourse-technician.rb
@@ -2,3 +2,4 @@
 require_relative 'concourse-technician/cli'
 require_relative 'concourse-technician/database'
 require_relative 'concourse-technician/settings'
+require_relative 'concourse-technician/volume_reaper'
